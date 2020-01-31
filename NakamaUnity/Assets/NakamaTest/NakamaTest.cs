81c98161d81f26b08f8cdf3d99e7dbd6343cadcb
Monkey D. Luffy <luffy531@protonmail.com>
2020-01-31 12:24:29 +0530

    First

---
+++ NakamaUnity/Assets/NakamaTest/NakamaTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Nakama;
+public class NakamaTest : MonoBehaviour
+{
+    public bool runValidate;
+
+    ISession session;
+    IApiAccount account;
+    void Start()
+    {
+        EstablishConnection();
+    }
+
+    private void OnValidate() {
+        if(runValidate)
+        {
+            runValidate = false;
+            session = null;
+            PlayerPrefs.SetString("nakama.session", "");
+            EstablishConnection();
+        }
+    }
+
+    async void EstablishConnection()
+    {
+        var client = new Client("http", "127.0.0.1", 7350, "defaultkey");
+
+        // Restore.
+        var authtoken = PlayerPrefs.GetString("nakama.session");
+        session = Session.Restore(authtoken);
+
+        try
+        {
+            account = await client.GetAccountAsync(session);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+
+            var deviceId = SystemInfo.deviceUniqueIdentifier;
+            session = await client.AuthenticateDeviceAsync(deviceId);
+            account = await client.GetAccountAsync(session);
+
+            PlayerPrefs.SetString("nakama.session", session.AuthToken);
+        }
+        
+        Debug.Log($"User ID = {session.UserId}");   // "ea1e7609-372a-4d67-a495-58f955f3328b"
+        Debug.Log($"UserName = {session.Username}"); // "wRkuUTbKmY"
+        Debug.Log($"Accout User ID = {account.User.Id}"); // "ea1e7609-372a-4d67-a495-58f955f3328b"
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Nakama
+
 Nakama Test With Server using Unity3D
+
+
+
+#### Start Docker
+
+Go to `NakamaDocker` use
+
+```bash
+docker-compose up
+```
+
+**Open this link in browser**
+
+http://127.0.0.1:7351
+
+username => `admin`
+
+password => `password`
+
+![Nakama Login](NakamaLogin.png)
